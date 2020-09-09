1a3f315eae7ae5896ca59ee84736252beaaab7b7
PalumboN <nahuel.palumbo@gmail.com>
2020-09-09 13:26:19 -0300

    Adding cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,20 @@
 import * as express from 'express'
+import * as cors from 'cors'
 import serverErrorHandler from './errorHandlers'
 import users from './users'
 
 const router = express.Router()
 
 router.use(express.json())
+router.use(cors())
 router.all('/ping', (_, res) => res.send('pong'))
 router.use(users)
+
+router.post('/challenges', (req, res) => {
+  console.log(req.body)
+  res.end()
+})
+
 router.use(serverErrorHandler)
 
 export default router
