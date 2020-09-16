097c5299056a9b40559941aea487acf202ac437f
PalumboN <nahuel.palumbo@gmail.com>
2020-09-16 18:53:03 -0300

    Hi ResourceRequest

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -1,11 +1,10 @@
 import Challenge from '../models/challenge'
 import Solution from '../models/solution'
 import * as express from 'express'
-import { syncHandler } from './utils'
+import { syncHandler, ResourceRequest } from './utils'
 import { EntityNotFound } from './errorHandlers'
-import * as mongoose from 'mongoose'
 
-type ChallengeRequest = express.Request & { solution: mongoose.Document } //TODO: ResourceRequest<field>
+type ChallengeRequest = ResourceRequest<'solution'>
 
 const router = express.Router()
 
